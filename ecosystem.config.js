af9ed4b8456c2d4f0cc04df9ab59603d03a07926
Arunkumar Selvaraj <arun@ioak.io>
2024-07-10 11:31:10 +0530

    deployment changes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
       {
         name: 'next-app',
         script: 'serve',
-        args: 'out -l 3000',
+        args: 'out -l 7001', // Change 'out' to your build directory if different
         env: {
           NODE_ENV: 'production',
           PORT: 7001
