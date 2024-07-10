272a9f7ca7e5a6bce136f8be630017503afb510a
Arunkumar Selvaraj <arun@ioak.io>
2024-07-10 16:39:46 +0530

    Deployment configuration changes

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 module.exports = {
   env: {
-    API_URL: process.env.NEXT_PUBLIC_API_URL,
+    API_URL: process.env.API_URL,
   },
   trailingSlash: true,
   experimental: {
