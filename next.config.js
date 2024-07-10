c04f292eb62527ab0602e7da86eede42cd8b1d3e
Arunkumar Selvaraj <arun@ioak.io>
2024-07-10 16:29:47 +0530

    Deployment configuration changes

--- next.config.js
+++ next.config.js
@@ -1,22 +1,21 @@
-const path = require('path')
-const dotenv = require('dotenv')
+const dotenv = require("dotenv");
+const path = require("path");
 
-let envFile = '.env';
-if (process.env.ENVIRONMENT) {
-  envFile = `.env.${process.env.ENVIRONMENT}`;
-}
-dotenv.config({
-  path: path.join(__dirname, envFile),
-});
+// Load environment variables from .env.production
+dotenv.config({ path: path.resolve(process.cwd(), ".env.production") });
 
 module.exports = {
+  env: {
+    API_URL: process.env.NEXT_PUBLIC_API_URL,
+    NODE_ENV: process.env.NODE_ENV,
+  },
   trailingSlash: true,
   experimental: {
     missingSuspenseWithCSRBailout: false,
   },
   sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
+    includePaths: [path.join(__dirname, "styles")],
   },
   // output: "export",
   // reactStrictMode: false
-}
+};
